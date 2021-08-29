bef6e7644de8324680fb4cc4a0665e93c72490b2
yilmazme <10myilmaz@gmail.com>
2021-08-30 00:36:47 +0300

    comment section fixed

--- routes/articles.js
+++ routes/articles.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
 require('dotenv').config();
 
 
@@ -8,7 +9,6 @@ const Story = require("../Story");
 
 var loggedin=false;
 var newComment= false;
-var commentArray=["0"];
 
 //this func prevent cache-- when user logout can go back authenticated page page
 router.use(function(req, res, next) {
@@ -22,7 +22,7 @@ router.get("/articles", (req,res)=>{
     .lean()
     .then((data)=>{
       
-        res.render("home",{story:data, loggedin:loggedin, style:"home.css"})
+        res.render("home",{story:data, loggedin:loggedin, style:"css/home.css"})
     })
     
 })
@@ -35,7 +35,7 @@ router.post("/articles/logout",(req,res)=>{
 
 router.get("/articles/login", (req,res)=>{
     if(!loggedin){
-        res.render("login", {style:"../login.css"})
+        res.render("login", {style:"../css/login.css"})
     }
     else{
         res.redirect("back")
@@ -63,10 +63,10 @@ function checkLoggedin(req,res,next){
 }
 
 router.get("/articles/new",checkLoggedin ,(req,res)=>{
-    res.render("form",{newComment:newComment, idx:commentArray[commentArray.length-1]})
+    res.render("form",{newComment:newComment})
     setTimeout(()=>{
          newComment=false;
-        commentArray.pop();
+
         }, 4000)
 })
 
@@ -88,6 +88,15 @@ newStory
 res.redirect("/articles")
 })
 
+//to see comments
+router.get("/articles/comments", checkLoggedin, (req,res)=>{
+    fs.readFile("comment.txt","utf-8",(err,html)=>{
+        if(err) throw err;
+        res.send(html)
+    })
+})
+
+
 router.get("/articles/:id",(req,res)=>{
    Story.findById(req.params.id)
    .lean()
@@ -95,8 +104,8 @@ router.get("/articles/:id",(req,res)=>{
        res.render("story",{
             story:data,
             loggedin:loggedin,
-            style:"../story.css",
-            text:"Yorum yap\n \n(anonim yorum yaptığınızda yorumunuzu ancak blogun sahibi silebilir)"
+            style:"../css/story.css",
+            text:"Yorum yap"
        })
    })
    .catch(() => {
@@ -114,15 +123,30 @@ router.post("/articles/:id", (req,res)=>{
         test = test +" "+comment1[i].replace(/[^a-zA-Z0-9,.ışöüğç ]/g, '')
  
       }
+      if(loggedin){
         Story.findByIdAndUpdate(req.params.id,{$push:{comments:test}},{new:true},(err, doc)=>{
             if (err) {
-                console.log("Something wrong when updating data!");
+                console.log("Something went wrong when updating data!");
             }
             res.redirect("back")
             return;
         })
-      newComment=true;
-      commentArray.push(req.params.id) 
+      }
+      else{
+          let tarih = new Date().toLocaleString();
+          let myTemplate = `<h3>Date:${tarih}</h3><br/> <p>COMMENT: ${test}</p> 
+          <p>Story Id: ${req.params.id}</p>
+          <p>Gönderen Ip: ${req.ip}</p>`
+        fs.appendFile("comment.txt",myTemplate, (err)=>{
+           if(err) throw err;
+           console.log("saved")
+           res.redirect("back")
+           newComment=true;
+           return;
+        })
+
+      }
+      
 })
 
 //updating likes
@@ -130,10 +154,11 @@ router.put("/articles/:id", (req,res)=>{
     const {likes} = req.body;
     Story.findByIdAndUpdate(req.params.id,{$set:{likes: Number(likes)+1}},{new:true},(err, doc)=>{
         if (err) {
-            console.log("Something wrong when updating data!");
+            console.log("Something went wrong when updating data!");
         }
         res.redirect("back")
     })    
 })
+
 
 module.exports=router;
