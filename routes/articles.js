9fa05bb2547a58379c69ced446337b29889f7699
yilmazme <10myilmaz@gmail.com>
2021-08-30 02:24:47 +0300

    path fixed

--- routes/articles.js
+++ routes/articles.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 require('dotenv').config();
-const path = require("path");
 
 
 const Story = require("../Story");
