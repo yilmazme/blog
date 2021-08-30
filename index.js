b8e7d7d3f14e45e5fefa808c506ac5e03f2b4f8d
yilmazme <10myilmaz@gmail.com>
2021-08-30 13:08:44 +0300

    public path fixed

--- index.js
+++ index.js
@@ -3,40 +3,36 @@ const path = require("path");
 const exphbs = require("express-handlebars");
 const mongoose = require("mongoose");
 const router = require("./routes/articles");
-const methodOver= require("method-override")
-require('dotenv').config();
-
-
+const methodOver = require("method-override");
+require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
 
-app.use(methodOver('_method'));
+app.use(methodOver("_method"));
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
+mongoose.connect(process.env.MONGODB_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
 
-
-
-mongoose.connect(process.env.MONGODB_URI,{
-    useUnifiedTopology:true,
-    useNewUrlParser:true,
-    useFindAndModify:false,
-})
-
-mongoose.connection.on("connected",()=>{
-    console.log("db connected")
-})
-app.use("/", router );
-app.get("*", (req,res)=>{
-    res.send("<h2>There is no such path</h2>")
-})
-const port = process.env.PORT || 3000
-app.listen(port,()=>{
-    console.log(`app started at ${port}`)
-})
+mongoose.connection.on("connected", () => {
+  console.log("db connected");
+});
+app.use("/", router);
+app.get("*", (req, res) => {
+  res.send(req.path);
+  //res.send("<h2>There is no such path</h2>");
+});
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`app started at ${port}`);
+});
--- routes/articles.js
+++ routes/articles.js
@@ -1,163 +1,174 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
-require('dotenv').config();
-
+require("dotenv").config();
 
 const Story = require("../Story");
 
-var loggedin=false;
-var newComment= false;
+var loggedin = false;
+var newComment = false;
 
 //this func prevent cache-- when user logout can go back authenticated page page
-router.use(function(req, res, next) {
-      res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
-      next();
-    });
- 
-router.get("/articles", (req,res)=>{
-    
-    Story.find({})
+router.use(function (req, res, next) {
+  res.header(
+    "Cache-Control",
+    "no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0"
+  );
+  next();
+});
+
+router.get("/articles", (req, res) => {
+  Story.find({})
     .lean()
-    .then((data)=>{
-      
-        res.render("home",{story:data, loggedin:loggedin, style:"home.css"})
-    })
-    
-})
+    .then((data) => {
+      res.render("home", {
+        story: data,
+        loggedin: loggedin,
+        style: "home.css",
+      });
+    });
+});
 
-router.post("/articles/logout",(req,res)=>{
-    loggedin=false;
-    res.redirect("/articles");
-    
-})
+router.post("/articles/logout", (req, res) => {
+  loggedin = false;
+  res.redirect("/articles");
+});
 
-router.get("/articles/login", (req,res)=>{
-    if(!loggedin){
-        res.render("login", {style:"../login.css"})
-    }
-    else{
-        res.redirect("back")
-        return
-    }
-})
+router.get("/articles/login", (req, res) => {
+  if (!loggedin) {
+    res.render("login", { style: "login.css" });
+  } else {
+    res.redirect("back");
+    return;
+  }
+});
 
-     function admin(req, res, next) {
-      const {username, password}=req.body;
-      if (username != process.env.USER_NAME || password != process.env.LOGIN_PASSWORD) {
-        return res.status(403).send({error: { status:403, message:'Access denied.'}});
-      }
-      loggedin=true;
-      next();
-    }
-  
-router.post("/articles/login",admin,(req,res)=>{
-    res.redirect("/articles/new")
-})
-function checkLoggedin(req,res,next){
-    if(!loggedin){
-        return res.status(403).send({error: { status:403, message:'Access denied.'}});
-    }
-    next()
+function admin(req, res, next) {
+  const { username, password } = req.body;
+  if (
+    username != process.env.USER_NAME ||
+    password != process.env.LOGIN_PASSWORD
+  ) {
+    return res
+      .status(403)
+      .send({ error: { status: 403, message: "Access denied." } });
+  }
+  loggedin = true;
+  next();
 }
 
-router.get("/articles/new",checkLoggedin ,(req,res)=>{
-    res.render("form",{newComment:newComment})
-    setTimeout(()=>{
-         newComment=false;
+router.post("/articles/login", admin, (req, res) => {
+  res.redirect("/articles/new");
+});
+function checkLoggedin(req, res, next) {
+  if (!loggedin) {
+    return res
+      .status(403)
+      .send({ error: { status: 403, message: "Access denied." } });
+  }
+  next();
+}
 
-        }, 4000)
-})
+router.get("/articles/new", checkLoggedin, (req, res) => {
+  res.render("form", { newComment: newComment });
+  setTimeout(() => {
+    newComment = false;
+  }, 4000);
+});
 
-router.post("/articles/new", (req,res)=>{
-const {name,date,description,story, comment, like}=req.body;
-const newStory = new Story({
-    name:name,
+router.post("/articles/new", (req, res) => {
+  const { name, date, description, story, comment, like } = req.body;
+  const newStory = new Story({
+    name: name,
     date: date,
-    description:description,
+    description: description,
     story: story,
-    comments:comment,
-    likes: like
-})
-newStory
-.save()
-.then((data)=>{
-    console.log(data)
-})
-res.redirect("/articles")
-})
+    comments: comment,
+    likes: like,
+  });
+  newStory.save().then((data) => {
+    console.log(data);
+  });
+  res.redirect("/articles");
+});
 
 //to see comments
-router.get("/articles/comments", checkLoggedin, (req,res)=>{
-    fs.readFile("comment.txt","utf-8",(err,html)=>{
-        if(err) throw err;
-        res.send(html)
+router.get("/articles/comments", (req, res) => {
+  fs.readFile(__dirname + "/comment.txt", "utf-8", (err, html) => {
+    if (err) {
+      console.log(err);
+      res.redirect("back");
+    }
+    res.send(html);
+  });
+});
+
+router.get("/articles/:id", (req, res) => {
+  Story.findById(req.params.id)
+    .lean()
+    .then((data) => {
+      res.render("story", {
+        story: data,
+        loggedin: loggedin,
+        style: "story.css",
+        text: "Yorum yap",
+      });
     })
-})
+    .catch(() => {
+      res.send(`<h2>There is no article with id:${req.params.id}</h2>`);
+    });
+});
 
-
-router.get("/articles/:id",(req,res)=>{
-   Story.findById(req.params.id)
-   .lean()
-   .then((data)=>{
-       res.render("story",{
-            story:data,
-            loggedin:loggedin,
-            style:"../story.css",
-            text:"Yorum yap"
-       })
-   })
-   .catch(() => {
-       res.send(`<h2>There is no article with id:${req.params.id}</h2>`)
-   });
-})
-
-  
-
-router.post("/articles/:id", (req,res)=>{
-    let {comment} = req.body;
-    let comment1 =comment.toLowerCase().split(" ")
-    let test="";
-    for(let i=0; i<comment1.length; i++){
-        test = test +" "+comment1[i].replace(/[^a-zA-Z0-9,.ışöüğç ]/g, '')
- 
+router.post("/articles/:id", (req, res) => {
+  let { comment } = req.body;
+  let comment1 = comment.toLowerCase().split(" ");
+  let test = "";
+  for (let i = 0; i < comment1.length; i++) {
+    test = test + " " + comment1[i].replace(/[^a-zA-Z0-9,.ışöüğç ]/g, "");
+  }
+  if (loggedin) {
+    Story.findByIdAndUpdate(
+      req.params.id,
+      { $push: { comments: test } },
+      { new: true },
+      (err, doc) => {
+        if (err) {
+          console.log("Something went wrong when updating data!");
+        }
+        res.redirect("back");
+        return;
       }
-      if(loggedin){
-        Story.findByIdAndUpdate(req.params.id,{$push:{comments:test}},{new:true},(err, doc)=>{
-            if (err) {
-                console.log("Something went wrong when updating data!");
-            }
-            res.redirect("back")
-            return;
-        })
-      }
-      else{
-          let tarih = new Date().toLocaleString();
-          let myTemplate = `<h3>Date:${tarih}</h3><br/> <p>COMMENT: ${test}</p> 
+    );
+  } else {
+    let tarih = new Date().toLocaleString();
+    let myTemplate = `<h3>Date:${tarih}</h3><br/> <p>COMMENT: ${test}</p> 
           <p>Story Id: ${req.params.id}</p>
-          <p>Gönderen Ip: ${req.ip}</p>`
-        fs.appendFile("comment.txt",myTemplate, (err)=>{
-           if(err) throw err;
-           console.log("saved")
-           res.redirect("back")
-           newComment=true;
-           return;
-        })
-
-      }
-      
-})
+          <p>Gönderen Ip: ${req.ip}</p>`;
+    let path = __dirname + "/comment.txt";
+    fs.appendFile(path, myTemplate, (err) => {
+      if (err) throw err;
+      console.log("saved");
+      res.redirect("back");
+      newComment = true;
+      return;
+    });
+  }
+});
 
 //updating likes
-router.put("/articles/:id", (req,res)=>{
-    const {likes} = req.body;
-    Story.findByIdAndUpdate(req.params.id,{$set:{likes: Number(likes)+1}},{new:true},(err, doc)=>{
-        if (err) {
-            console.log("Something went wrong when updating data!");
-        }
-        res.redirect("back")
-    })    
-})
+router.put("/articles/:id", (req, res) => {
+  const { likes } = req.body;
+  Story.findByIdAndUpdate(
+    req.params.id,
+    { $set: { likes: Number(likes) + 1 } },
+    { new: true },
+    (err, doc) => {
+      if (err) {
+        console.log("Something went wrong when updating data!");
+      }
+      res.redirect("back");
+    }
+  );
+});
 
-
-module.exports=router;
+module.exports = router;
